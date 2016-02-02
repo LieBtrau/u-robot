bb686bfec9c6edf6456f33ff35b42625387f0a24
Christoph Tack <prog.send@gmail.com>
2016-02-02 21:30:30 +0100

    Adding due time, parsing of events
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- python/u-robot.py
+++ python/u-robot.py
@@ -1,39 +1,79 @@
-#Info
+# Info
 #	Python Exchange Web services library: https://pyexchange.readthedocs.org/en/latest/
-#Installation
+# Installation
 #	sudo apt-get install libxml2 libxml2-dev libxslt1-dev python-pip python-dev zlib1g-dev && pip install lxml pyexchange
 #	download: https://pypi.python.org/packages/source/t/tzlocal/tzlocal-1.2.tar.gz
 #	extract it and run sudo python setup.py install
 from pyexchange import Exchange2010Service, ExchangeNTLMAuthConnection
 from pytz import timezone
-from datetime import datetime
+from datetime import datetime, timedelta
 from tzlocal import get_localzone
-import getpass, ConfigParser
+import getpass, ConfigParser, subprocess
 
-#The code is open source, but the url, username and password are not.
-#The url and username are stored in a separate config file.
+
+def speak(sentence):
+    p = subprocess.Popen('pico2wave -w lookdave.wav \'' + sentence + '\' && aplay lookdave.wav', shell=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    retval = p.wait()
+    return retval
+
+
+def getCalendarEvents(url, username, password, startdate, duration):
+    # Set up the connection to Exchange
+    connection = ExchangeNTLMAuthConnection(url=url,
+                                            username=username,
+                                            password=password)
+    service = Exchange2010Service(connection)
+    events = service.calendar().list_events(
+        start=get_localzone().localize(startdate),
+        end=get_localzone().localize(startdate + duration),
+        details=True
+    )
+    return events
+
+
+def validEvent(event):
+    return not str(event.subject).startswith('Canceled')
+
+
+def speakEvent(event):
+    if str(event.location) == 'None':
+        return
+    sentence = TITLE + ', you have a ' + event.subject + ' meeting.\n'
+    duetime = event.start - datetime.now(get_localzone())
+    sentence += 'This meeting will start in {0} hours.'.format(int(duetime.total_seconds() / 3600))
+    speak(sentence)
+
+
+# The code is open source, but the url, username and password are not.
+# The url and username are stored in a separate config file.
 config = ConfigParser.RawConfigParser()
 config.read('example.cfg')
-URL = unicode(config.get('Section1', 'ExchangeServerUrl'))
-USERNAME = unicode(config.get('Section1', 'UserName'))
-#The password is read at startup of the program.
+url = unicode(config.get('Section1', 'ExchangeServerUrl'))
+username = unicode(config.get('Section1', 'UserName'))
+# The password is read at startup of the program.
 PASSWORD = getpass.getpass('Password: ')
+TITLE = 'Master'
 
-# Set up the connection to Exchange
-connection = ExchangeNTLMAuthConnection(url=URL,
-                                        username=USERNAME,
-                                        password=PASSWORD)
-service = Exchange2010Service(connection)
+try:
+    eventList = getCalendarEvents(url=url,
+                                  username=username,
+                                  password=PASSWORD,
+                                  startdate=datetime.now(),
+                                  duration=timedelta(days=1))
+except:
+    speak('I am sorry '+ TITLE + ' , but I have no access to your calendar.')
+    exit()
 
-events = service.calendar().list_events(
-    start=get_localzone().localize(datetime(2015, 10, 1, 11, 0, 0)),
-    end=get_localzone().localize(datetime(2015, 10, 29, 11, 0, 0)),
-    details=True
-)
+if eventList.count == 0:
+    speak('You are so lucky today '+ TITLE + '.  There are no meetings scheduled for you.')
 
-for event in events.events:
-    print "{start} {stop} - {subject}".format(
+for event in eventList.events:
+    s = "{start} {stop} - {subject} - {location}".format(
         start=event.start,
         stop=event.end,
-        subject=event.subject
-    )
+        subject=event.subject,
+        location=event.location)
+    print s
+    if (validEvent(event)):
+        speakEvent(event)
