4f7d619c4327e8795f13c2d015d4401be9dec9d9
Christoph Tack <prog.send@gmail.com>
2022-06-14 20:23:13 +0200

    ExchangeLib access migrated to separate class

--- python/u-robot_exchangelib.py
+++ python/u-robot_exchangelib.py
@@ -1,56 +1,18 @@
-#!/usr/bin/env python3
-#
-# Authentication to Microsoft 365:
-#   - Basic authentication (username/password) is being deprecated in favor of OAuth-tokens
-#   - For using OAuth, this application must first be registered with Azure Active Directory
-#
-# References:
-#   https://docs.microsoft.com/en-us/exchange/client-developer/exchange-web-services/how-to-authenticate-an-ews-application-by-using-oauth
-#   https://ecederstrand.github.io/exchangelib/#oauth-authentication
-#
-#
-# Installation:
-# pip install exchangelib
-#
 
-from calendar import month
-import configparser
+
 import logging
 import sys
 import datetime
 from tzlocal import get_localzone
-from exchangelib import OAuth2Credentials, DELEGATE, Account, Configuration, Identity
-
+import CalendarExchangeLib
 
 def main(argv):
-    config = configparser.RawConfigParser()
-    config.read('example.cfg')
-    username = str(config.get('Section1', 'UserName'))
-
-    credentials = OAuth2Credentials(
-        client_id=str(config.get('Section1', 'client_id')),
-        client_secret = str(config.get('Section1', 'client_secret')),
-        tenant_id=str(config.get('Section1', 'tenant_id')),
-        identity=Identity(primary_smtp_address=username)
-    )
-
-    config = Configuration(server='outlook.office365.com', credentials=credentials)
-
-    test_account = Account(
-        primary_smtp_address = username,
-        config = config,
-        autodiscover = False,
-        access_type = DELEGATE
-    )
 
     start = datetime.datetime.now(get_localzone())
-    items = test_account.calendar.view(
-        start=start,
-        end=start + datetime.timedelta(weeks=4),
-    )
+    myCalendar = CalendarExchangeLib.CalendarExchangeLib('example.cfg')
+    items = myCalendar.getCalendarEvents(start, datetime.timedelta(weeks = 2))
     for item in items:
         print(item.start, item.end, item.subject)
-
 
 
 if __name__ == '__main__':
